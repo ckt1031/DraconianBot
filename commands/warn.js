5a9f13d4f1d1d92f771dbb1ffcf55e7b1e6355ea
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2020-12-27 16:33:44 +0800

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,84 +1,123 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const fs = require("fs");
 const ms = require("ms");
 //const mysql = require('mysql');
 //const file = require('../mysql.json');
 
 exports.run = async (client, message, args) => {
-  let reason = args.slice(1).join(' ');
+  let reason = args.slice(1).join(" ");
   let user = message.mentions.users.first();
   let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
+
+  let notice1 = new Discord.MessageEmbed()
+    .setDescription(
+      `<:cross1:747728200691482746> **${message.author.username}, Missing Permission**`
+    )
+    .setColor("GREEN");
+  let notice3 = new Discord.MessageEmbed()
+    .setDescription(
+      `<:cross1:747728200691482746> **I don't have permission to warn people!**`
+    )
+    .setColor("RED");
+  if (!message.guild.member(client.user).hasPermission("MANAGE_ROLES"))
+    return message.channel.send(notice3);
   //let logchannel = message.guild.channels.cache.find(x => x.name = 'logs');
-  if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("❌**Error:** You don't have the **Kick Members** permission!");
-  if (message.mentions.users.size < 1) return message.reply('You must mention someone to warn them.').catch(console.error);
-  if (message.mentions.users.first().id === message.author.id) return message.reply('You cannot warn yourself');
+  if (!message.member.hasPermission("KICK_MEMBERS"))
+    return message.channel.send(notice1);
+  if (message.mentions.users.size < 1)
+    return message
+      .reply("You must mention someone to warn them.")
+      .catch(console.error);
+  let notice2 = new Discord.MessageEmbed()
+    .setDescription(`<:cross1:747728200691482746> **You cannot warn yourself**`)
+    .setColor("RED");
+  if (message.mentions.users.first().id === message.author.id)
+    return message.channel.send(notice2);
   //if (!logchannel) return message.channel.send('I cannot find a logs channel');
-  if (reason.length < 1) reason = 'No reason supplied.';
-  
-  if(!warns[`${user.id}, ${message.guild.id}`]) warns[`${user.id}, ${message.guild.id}`] = {
-    warns: 0
-  };
+  if (reason.length < 1) reason = "No reason supplied.";
+
+  if (!warns[`${user.id}, ${message.guild.id}`])
+    warns[`${user.id}, ${message.guild.id}`] = {
+      warns: 0
+    };
 
   warns[`${user.id}, ${message.guild.id}`].warns++;
 
   fs.writeFile("./warnings.json", JSON.stringify(warns), err => {
-    if(err) throw err;
+    if (err) throw err;
   });
 
   const embed = new Discord.MessageEmbed()
-  .setColor(0xFFFF00)
-  .setTimestamp()
-  .addField('Action:', 'Warning')
-  .addField('User:', `${user.username}#${user.discriminator}`)
-  .addField('Warned by:', `${message.author.username}#${message.author.discriminator}`)
-  .addField('Number of warnings:', warns[`${user.id}, ${message.guild.id}`].warns)
-  .addField('Reason', reason)
-  let logchannel = message.guild.channels.cache.find(x => x.name = 'logs');
+    .setColor(0xffff00)
+    .setTimestamp()
+    .addField("Action:", "Warning")
+    .addField("User:", `${user.username}#${user.discriminator}`)
+    .addField(
+      "Warned by:",
+      `${message.author.username}#${message.author.discriminator}`
+    )
+    .addField(
+      "Number of warnings:",
+      warns[`${user.id}, ${message.guild.id}`].warns
+    )
+    .addField("Reason", reason);
+  let logchannel = message.guild.channels.cache.find(x => (x.name = "logs"));
   let test1 = new Discord.MessageEmbed()
-   .setDescription(`<:tick:702386031361523723> **Muted <@${user.id}> For 1 Hour** | **Reached Two Warnings**`)
-   .setColor('GREEN')
-   let bsuembed = new Discord.MessageEmbed()
-   .setDescription(`<:tick:702386031361523723> **Warned** ${user.username}#${user.discriminator} | **${reason}**`)
-   .setColor('GREEN')
-   message.delete()
-  message.channel.send(bsuembed)
-  if(user.bot) return;
-  message.mentions.users.first().send(`You are warned by ${message.author.username}#${message.author.discriminator}, ${reason}`).catch(e =>{
-    if(e) return 
-  });
+    .setDescription(
+      `<:tick:702386031361523723> **Muted <@${user.id}> For 1 Hour** | **Reached Two Warnings**`
+    )
+    .setColor("GREEN");
+  let bsuembed = new Discord.MessageEmbed()
+    .setDescription(
+      `<:tick:702386031361523723> **Warned ${user.username}#${user.discriminator}** | **${reason}**`
+    )
+    .setColor("GREEN");
+  message.delete();
+  message.channel.send(bsuembed);
+  if (user.bot) return;
+  message.mentions.users
+    .first()
+    .send(
+      `You are warned by ${message.author.username}#${message.author.discriminator}, ${reason}`
+    )
+    .catch(e => {
+      if (e) return;
+    });
 
-     
-     let test2 = new Discord.MessageEmbed()
-   .setDescription(`<:tick:702386031361523723> **Kicked ${user.username}#${user.discriminator}** | **Reached Warnings 3**`)
-   .setColor('GREEN')
-     let test3 = new Discord.MessageEmbed()
-   .setDescription(`<:tick:702386031361523723> **Banned ${user.username}#${user.discriminator}** | **Reached 5 Warnings**`)
-   .setColor('GREEN')
+  let test2 = new Discord.MessageEmbed()
+    .setDescription(
+      `<:tick:702386031361523723> **Kicked ${user.username}#${user.discriminator}** | **Reached Warnings 3**`
+    )
+    .setColor("GREEN");
+  let test3 = new Discord.MessageEmbed()
+    .setDescription(
+      `<:tick:702386031361523723> **Banned ${user.username}#${user.discriminator}** | **Reached 5 Warnings**`
+    )
+    .setColor("GREEN");
 
-
-
-  if(warns[`${user.id}, ${message.guild.id}`].warns == 2){
-      let muteRole = client.guilds.cache.get(message.guild.id).roles.cache.find(val => val.name === 'Muted');
+  if (warns[`${user.id}, ${message.guild.id}`].warns == 2) {
+    let muteRole = client.guilds.cache
+      .get(message.guild.id)
+      .roles.cache.find(val => val.name === "Muted");
 
     let mutetime = "60s";
     message.guild.members.get(user.id).addRole(muteRole.id);
-    message.channel.send(test1)
+    message.channel.send(test1);
 
-    setTimeout(function(){
-      message.guild.members.get(user.id).removeRole(muteRole.id)
-    }, ms(mutetime))
+    setTimeout(function() {
+      message.guild.members.get(user.id).removeRole(muteRole.id);
+    }, ms(mutetime));
   }
 
-  if(warns[`${user.id}, ${message.guild.id}`].warns == 3){
+  if (warns[`${user.id}, ${message.guild.id}`].warns == 3) {
     message.guild.member(user).kick(reason);
-    message.channel.send(test2)
+    message.channel.send(test2);
   }
 
-  if(warns[`${user.id}, ${message.guild.id}`].warns == 5){
+  if (warns[`${user.id}, ${message.guild.id}`].warns == 5) {
     message.guild.member(user).ban(reason);
     message.channel.send(test3);
   }
-
 };
 
 exports.conf = {
@@ -89,7 +128,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'warn',
-  description: 'Issues a warning to the mentioned user.',
-  usage: 'warn [mention] [reason]'
+  name: "warn",
+  description: "Issues a warning to the mentioned user.",
+  usage: "warn [mention] [reason]"
 };
