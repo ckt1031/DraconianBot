8ee4ae72568a5f108b5337184042327752f23c42
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2020-12-28 21:20:28 +0800

    Add files via upload

---
+++ shard.js
@@ -0,0 +1,17 @@
+const { ShardingManager } = require('discord.js');
+
+
+const shards = new ShardingManager('./app.js', {
+    token: process.env.TOKEN,
+    totalShards: 1
+});
+
+shards.on('launch', shard => {
+    console.log(`[${new Date().toString().split(" ", 5).join(" ")}] Launched shard #${shard.id}`);
+});
+
+shards.on('message', (shard, msg) => {
+    console.log(`[${new Date().toString().split(" ", 5).join(" ")}] #${shard.id} | ${msg._eval} | ${msg._result}`);
+});
+
+shards.spawn();
