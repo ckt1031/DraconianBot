b3d4ea894e8690ff9594fda44cc19a83c6db4ba1
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2021-02-01 16:05:54 +0800

    Delete guildDelete.js

--- events/guildDelete.js
+++
@@ -1,7 +0,0 @@
-const settings = require("../settings.js");
-exports.run = (client, guild) => {
-		client.settings.delete(guild.id);
-	client.user.setActivity(`RealKoolisw | ${client.guilds.cache.size} Servers`, {
-		type: 'LISTENING'
-	});
-}
