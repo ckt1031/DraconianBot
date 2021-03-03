0ccd00616509ebcb6acae3a00865390b7efb45b1
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2021-03-03 22:26:17 +0800

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "airbnb-base",
+    "eslint:recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  ignorePatterns: ["index.js"],
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+  },
+};
