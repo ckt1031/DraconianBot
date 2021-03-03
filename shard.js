f98a94728cf828346f6e47cce08c95d15658c9a7
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2021-03-03 22:32:56 +0800

    Add files via upload

--- shard.js
+++ shard.js
@@ -1,26 +1,53 @@
-require("dotenv").config();
+require('dotenv').config()
 
 const { ShardingManager } = require("discord.js");
+const Statcord = require("statcord.js");
 
-const shards = new ShardingManager("./index.js", {
+const manager = new ShardingManager("./index.js", {
   token: process.env.TOKEN,
   totalShards: 1,
 });
 
-shards.on("launch", (shard) => {
+const statcord = new Statcord.ShardingClient({
+  key: process.env.STATCORD_API,
+  manager,
+  postCpuStatistics: true,
+  postMemStatistics: true,
+  postNetworkStatistics: true,
+  autopost: true,
+});
+
+statcord.registerCustomFieldHandler(1, async (manager) => {
+  console.log("Successful posted handler 1");
+});
+
+statcord.registerCustomFieldHandler(2, async (manager) => {
+  console.log("Successful posted handler 2");
+});
+
+manager.on("launch", (shard) => {
   console.log(
     `[${new Date().toString().split(" ", 5).join(" ")}] Launched shard #${
       shard.id
-    }`
+    }`,
   );
 });
 
-shards.on("message", (shard, msg) => {
+manager.on("message", (shard, msg) => {
   console.log(
     `[${new Date().toString().split(" ", 5).join(" ")}] #${shard.id} | ${
       msg._eval
-    } | ${msg._result}`
+    } | ${msg._result}`,
   );
 });
 
-shards.spawn();
+manager.spawn();
+
+statcord.on("autopost-start", () => {
+  console.log("Started autopost");
+});
+
+statcord.on("post", (status) => {
+  if (!status) console.log("Successful post");
+  else console.error(status);
+});
