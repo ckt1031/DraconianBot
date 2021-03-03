fd48b8354fd83878650ac9573634e6e7a3937a19
RealKoolisw <RealKoolisw@users.noreply.github.com>
2021-03-03 14:33:31 +0000

    Prettified Code!

--- shard.js
+++ shard.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const { ShardingManager } = require("discord.js");
 const Statcord = require("statcord.js");
@@ -29,7 +29,7 @@ manager.on("launch", (shard) => {
   console.log(
     `[${new Date().toString().split(" ", 5).join(" ")}] Launched shard #${
       shard.id
-    }`,
+    }`
   );
 });
 
@@ -37,7 +37,7 @@ manager.on("message", (shard, msg) => {
   console.log(
     `[${new Date().toString().split(" ", 5).join(" ")}] #${shard.id} | ${
       msg._eval
-    } | ${msg._result}`,
+    } | ${msg._result}`
   );
 });
 
