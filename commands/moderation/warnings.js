f994738f1aa0d9403102fb3c9811911bfcdac80e
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2021-03-04 15:11:28 +0800

    Update warnings.js

--- commands/moderation/warnings.js
+++ commands/moderation/warnings.js
@@ -3,6 +3,12 @@ const fs = require("fs");
 const ms = require("ms");
 
 module.exports.run = async (client, message, args) => {
+	const emddd = new Discord.MessageEmbed()
+		.setDescription(
+			"<:cross1:747728200691482746> **Couldn't find that user...!**"
+		)
+		.setColor("RED");
+
 	const warns = JSON.parse(
 		fs.readFileSync("./temp-datastore/warnings.json", "utf8")
 	);
@@ -12,17 +18,18 @@ module.exports.run = async (client, message, args) => {
 			.reply("You must mention someone to check their warns.")
 			.catch(console.error);
 	}
-	if (!user) return message.reply("Couldn't find that user...");
-	if (!warns[user.id]) {
-		warns[user.id] = {
+	if (!user) return message.channel.send(emddd);
+	if (!warns[`${user.id}, ${message.guild.id}`]) {
+		warns[`${user.id}, ${message.guild.id}`] = {
 			warns: 0,
 		};
 	}
+	// if (!warns[user.id]) return message.channel.send(emddd)
 
 	const embed = new Discord.MessageEmbed()
 		.setColor("GREEN")
 		.setTimestamp()
-		.addField("Action:", "Warn Check")
+		.setTitle("<:tick:702386031361523723> Warn Check")
 		.addField("User:", `${user.username}#${user.discriminator}`)
 		.addField(
 			"Number of warnings:",
