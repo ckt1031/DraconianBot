ad22fb716b80cb19a5ff2c1d9c427baa2f832bed
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2021-03-08 04:32:42 +0000

    Add files via upload

--- commands/moderation/unban.js
+++ commands/moderation/unban.js
@@ -2,32 +2,32 @@ const Discord = require("discord.js");
 const config = require("../../config.json");
 
 module.exports.run = (client, message, args) => {
-	let reason = args.slice(1).join(" ");
-	let user = args[0];
+	const reason = args.slice(1).join(" ");
+	const user = args[0];
 
-	let embed1 = new Discord.MessageEmbed()
+	const embed1 = new Discord.MessageEmbed()
 		.setTitle("Error")
 		.setDescription("**The user you entered has not been banned!**")
 		.setColor("RED");
-	let embed2 = new Discord.MessageEmbed()
+	const embed2 = new Discord.MessageEmbed()
 		.setDescription("you must supply a `UserResolvable`, i.e. a user ID.")
 		.setColor("RED");
-
-	let EMDDD = new Discord.MessageEmbed()
-		.setDescription(`<:tick:702386031361523723> **Unbanned** ${user.id}`)
-		.setColor("#FFFF00");
-
-	let userID = args[0];
+	const userID = args[0];
 	message.guild.fetchBans().then(bans => {
-		if (bans.size == 0)
+		if (bans.size == 0) {
 			return message.channel
 				.send(embed1)
 				.then(m => m.delete({ timeout: 15000 }));
-		let bUser = bans.find(b => b.user.id == userID);
-		if (!bUser)
+		}
+		const bUser = bans.find(b => b.user.id == userID);
+		if (!bUser) {
 			return message.channel
 				.send(embed2)
 				.then(m => m.delete({ timeout: 15000 }));
+		}
+		const EMDDD = new Discord.MessageEmbed()
+			.setDescription(`<:tick:702386031361523723> **Unbanned** **${args[0]}**`)
+			.setColor("#FFFF00");
 		message.channel.send(EMDDD);
 		message.guild.members.unban(bUser.user);
 	});
