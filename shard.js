7f1817a61d85a7207583b8843a1bc36d56c81b48
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2021-03-24 23:29:49 +0800

    Update shard.js

--- shard.js
+++ shard.js
@@ -11,12 +11,12 @@ require("dotenv").config();
 
 const { ShardingManager } = require("discord.js");
 
-const manager = new ShardingManager("./index.js", {
+const shard = new ShardingManager("./index.js", {
 	token: process.env.TOKEN,
 	totalShards: 1,
 });
 
-manager.on("launch", shard => {
+shard.on("launch", shard => {
 	console.log(
 		`[${new Date().toString().split(" ", 5).join(" ")}] Launched shard #${
 			shard.id
@@ -24,7 +24,7 @@ manager.on("launch", shard => {
 	);
 });
 
-manager.on("message", (shard, msg) => {
+shard.on("message", (shard, msg) => {
 	console.log(
 		`[${new Date().toString().split(" ", 5).join(" ")}] #${shard.id} | ${
 			msg._eval
@@ -32,4 +32,4 @@ manager.on("message", (shard, msg) => {
 	);
 });
 
-manager.spawn();
+shard.spawn();
