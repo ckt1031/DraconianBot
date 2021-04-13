136fa9efe80a8704783c4333862320c739127aab
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2021-04-13 20:56:47 +0800

    Create petpet.js

---
+++ commands/images/petpet.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const petPetGif = require("pet-pet-gif");
+
+module.exports.run = async (client, message, args) => {
+  const target = message.mentions.users.first() || message.author;
+
+  const avatar = message.attachments.array()[0];
+
+  if (avatar) {
+    if (avatar.url) {
+      const image = await petPetGif(avatar.url);
+      const rainbow = new Discord.MessageAttachment(image, "petpet.gif");
+      return message.channel.send(rainbow);
+    }
+  } else {
+    const image = await petPetGif(
+      target.displayAvatarURL({ dynamic: false, format: "png" }),
+    );
+    const rainbow = new Discord.MessageAttachment(image, "petpet.gif");
+    return message.channel.send(rainbow);
+  }
+};
+
+module.exports.help = {
+  name: "petpet",
+  description: "Generate Pet Pet pictures.",
+  usage: "d!petpet <mentions / attachments>(optional)",
+  accessableby: "Member",
+  aliases: [],
+};
