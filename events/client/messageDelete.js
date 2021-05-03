649c3b7aeb085628e75ffe0523d09a7783a84aff
RealKoolisw <RealKoolisw@users.noreply.github.com>
2021-05-03 01:46:18 +0000

    Formatted Code!

--- events/client/messageDelete.js
+++ events/client/messageDelete.js
@@ -1,10 +1,10 @@
 module.exports = function (client, message, channel) {
-	if(!message.author) return
-  client.snipes.set(message.channel.id, {
-    content: message.content,
-    author: message.author.username+'#'+message.author.discriminator,
-    image: message.attachments.first()
-      ? message.attachments.first().proxyURL
-      : null,
-  });
+	if (!message.author) return;
+	client.snipes.set(message.channel.id, {
+		content: message.content,
+		author: message.author.username + "#" + message.author.discriminator,
+		image: message.attachments.first()
+			? message.attachments.first().proxyURL
+			: null,
+	});
 };
