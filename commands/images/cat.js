b7cee4bd10b64fb2eabb31a3173a3c6487a85a89
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2021-05-03 09:48:39 +0800

    Add files via upload

--- commands/images/cat.js
+++ commands/images/cat.js
@@ -1,15 +1,17 @@
-const request = require("request");
-const Discord = require("discord.js");
+const request = require('request');
+const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-	request("http://edgecats.net/random", function (error, response, body) {
+	request('http://edgecats.net/random', (error, response, body) => {
 		if (!error && response.statusCode == 200) {
-			let emb = new Discord.MessageEmbed()
+			const emb = new Discord.MessageEmbed()
 				.setImage(body)
-				.setColor("#00ff00")
-				.setTitle("Your random cat here")
+				.setColor('#00ff00')
+				.setTitle('Your random cat here')
 				.setFooter(
-					`©2020 Draconian Workshop | This command requested by ${message.author.username}#${message.author.discriminator}`
+					`©2020 Draconian Workshop | This command requested by ${
+						message.author.username
+					}#${message.author.discriminator}`
 				);
 
 			message.channel.send(emb);
@@ -18,9 +20,9 @@ module.exports.run = async (bot, message, args) => {
 };
 
 module.exports.help = {
-	name: "cat",
+	name: 'cat',
 	description: "This command is used for posting cat's images randomly.",
-	usage: "d!cat",
-	accessableby: "Members",
+	usage: 'd!cat',
+	accessableby: 'Members',
 	aliases: [],
 };
