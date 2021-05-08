cb18d881bcceed8c889af08fed4755986832e4a7
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2021-05-08 17:45:21 +0800

    Add files via upload

--- commands/images/dog.js
+++ commands/images/dog.js
@@ -2,12 +2,12 @@ const request = require("request");
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-	request("https://some-random-api.ml/img/cat", (error, _response, body) => {
+	request("https://some-random-api.ml/img/dog", (error, _response, body) => {
 		const json = JSON.parse(body);
 		const { link } = json;
 
 		const emb = new Discord.MessageEmbed();
-		emb.setDescription("Your Cat Image Here:");
+		emb.setDescription("Your Dog Image Here:");
 		emb.setColor("GREEN");
 		emb.setImage(link);
 
@@ -18,9 +18,9 @@ module.exports.run = async (bot, message, args) => {
 
 
 module.exports.help = {
-	name: "cat",
-	description: "This command is used for posting cat's images randomly.",
-	usage: "d!cat",
+	name: "dog",
+	description: "This command is used for posting dog's images randomly.",
+	usage: "d!dog",
 	accessableby: "Members",
 	aliases: [],
 };
