f3553493000e0b9194e4aea01528a0fc914f543f
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2021-05-13 16:01:32 +0800

    Update spank.js

--- commands/images/spank.js
+++ commands/images/spank.js
@@ -6,10 +6,6 @@ module.exports.run = async (client, message, args) => {
 		return message.reply("You need to mention someone to spank them");
 	if (!message.channel.nsfw)
 		return message.reply("No NO NO, NSFW is not enabled in this channel");
-	if (message.mentions.users.first().id === "242263403001937920")
-		return message.reply(
-			"You can't spank my Dev baka.:facepalm: He will spank your ass off >:3"
-		);
 	const { body } = await superagent.get("https://nekos.life/api/v2/img/spank");
 
 	const embed = new Discord.MessageEmbed()
