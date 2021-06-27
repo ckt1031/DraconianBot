19b393ae9ac29acee30ff0268175c089fed65356
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2021-06-27 14:59:21 +0800

    Update guildMemberAdd.js

--- events/client/guildMemberAdd.js
+++ events/client/guildMemberAdd.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const settings = require("../../settings.json");
+const settings = require("../../config/settings.json");
 
 module.exports = async (client, member) => {
 	client.settings.ensure(member.guild.id, settings);
