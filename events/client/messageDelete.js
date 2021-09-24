6409e39b620579b74215ae54ff9a82dd38dd85c7
RealKoolisw <65409152+RealKoolisw@users.noreply.github.com>
2021-09-24 23:08:07 +0800

    Update messageDelete.js

--- events/client/messageDelete.js
+++ events/client/messageDelete.js
@@ -1,10 +1,15 @@
-module.exports = function (client, message, channel) {
-	if (!message.author || !message.content) return;
-	return client.snipes.set(message.channel.id, {
-		content: message.content,
-		author: `${message.author.username}#${message.author.discriminator}`,
-		image: message.attachments.first()
-			? message.attachments.first().proxyURL
-			: null
-	});
+module.exports = function(client, message) {
+  const nowdate = new Date().toLocaleString("en-US", {
+    timeZone: "Asia/Hong_Kong"
+  });
+  let { author, content } = message;
+  if (!author || !content) return;
+  client.snipes.set(message.channel.id, {
+    content: content,
+    author: `${author.username}#${author.discriminator}`,
+    image: message.attachments.first()
+      ? message.attachments.first().proxyURL
+      : null,
+    date: nowdate
+  });
 };
