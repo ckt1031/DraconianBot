a16626bf477157db4aedcbd5e2d46c06a9cabb3b
cktsun1031 <cktsun1031@gmail.com>
2022-12-31 23:24:03 +0800

    chore: bump deps

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,13 +1,6 @@
-import rateLimit from '@fastify/rate-limit';
 import fastify from 'fastify';
 
 const server = fastify();
-
-server.register(rateLimit, {
-  global: true,
-  max: 60,
-  timeWindow: 60 * 1000 * 5,
-});
 
 server.get('/', async () => 'Draconian Bot');
 
