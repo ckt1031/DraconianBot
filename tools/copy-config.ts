81efaf734dca5c9bfe1bff5c3d6b16fe8f8b6c14
cktsun1031 <65409152+cktsun1031@users.noreply.github.com>
2023-02-12 15:06:47 +0800

    feat: optimized config merging

--- tools/copy-config.ts
+++ tools/copy-config.ts
@@ -1,10 +1,11 @@
-import { copyFileSync, existsSync } from 'node:fs';
+import { copyFileSync, existsSync, readFileSync, writeFileSync } from 'node:fs';
 import { join } from 'node:path';
 
 import chalk from 'chalk';
 import glob from 'glob';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import deepmerge from 'deepmerge';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -21,14 +22,33 @@ glob(folderPath, (error, allFiles) => {
 
   for (let index = 0, l = allFiles.length; index < l; index++) {
     const filePath = allFiles[index];
-    if (!existsSync(filePath.replace('.example', ''))) {
-      copyFileSync(filePath, filePath.replace('.example', ''));
-      console.log(chalk.green('COPY SUCCEED:') + ` ${filePath}`);
-    } else {
-      console.log(
-        chalk.red('COPY FAILED:') +
-          ` ${filePath} - Consider manually copy new updated option to old one.`,
-      );
+
+    // Path that used in code, user specified config path
+    const targetPath = filePath.replace('.example', '');
+
+    // The example config file
+    const defaultConfigFile = readFileSync(filePath);
+    const defaultConfig = JSON.parse(defaultConfigFile as unknown as string);
+
+    // If file exist, merge it
+    if (existsSync(targetPath)) {
+      // User specified config file
+      const oldConfigFile = readFileSync(targetPath);
+      const oldConfig = JSON.parse(oldConfigFile as unknown as string);
+
+      // Merged config
+      const newConfig = deepmerge(defaultConfig, oldConfig);
+
+      console.log(chalk.yellow('📀 MERGE SUCCEED:') + ` ${targetPath}`);
+
+      writeFileSync(targetPath, JSON.stringify(newConfig, null, 2));
+
+      continue;
     }
+
+    console.log(chalk.green('✅ COPY SUCCEED:') + ` ${targetPath}`);
+
+    // If file not exist, copy it
+    copyFileSync(filePath, targetPath);
   }
 });
